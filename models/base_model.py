35a4d84a75c072e6fa06cd90b22a20d313740069
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-10-03 01:43:58 +0300

    chore: update the base model and user.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains class BaseModel
+Module that contains the BaseModel class.
 """
 
 from datetime import datetime
@@ -10,7 +10,7 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
 if models.storage_t == "db":
     Base = declarative_base()
@@ -19,24 +19,51 @@ else:
 
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """
+    The BaseModel class from which future classes will be derived.
+
+    Attributes (if used with a database):
+        id (str): The unique identifier for the model instance.
+        created_at (datetime): The datetime when the instance was created.
+        updated_at (datetime): The datetime when the instance was last updated.
+
+    Methods:
+        __init__: Initializes a new BaseModel instance.
+        __str__: Returns a string representation of the BaseModel instance.
+        save: Updates the 'updated_at' attribute with the current datetime.
+        to_dict: Converts the BaseModel instance to a dictionary.
+        delete: Deletes the current instance from storage (if applicable).
+    """
+
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """
+        Initializes a new BaseModel instance.
+
+        Args:
+            *args: Positional arguments.
+            **kwargs: Keyword arguments.
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if kwargs.get("created_at", None) and isinstance(
+                    self.created_at, str):
+                self.created_at = datetime.strptime(
+                        kwargs["created_at"], time_format
+                    )
             else:
                 self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            if kwargs.get("updated_at", None) and isinstance(
+                    self.updated_at, str):
+                self.updated_at = datetime.strptime(
+                        kwargs["updated_at"], time_format
+                    )
             else:
                 self.updated_at = datetime.utcnow()
             if kwargs.get("id", None) is None:
@@ -47,7 +74,9 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation of the BaseModel class"""
+        """
+        Returns a string representation of the BaseModel instance.
+        """
         dictionary = self.__dict__.copy()
         if models.storage_t != 'db':
             dictionary.pop("_sa_instance_state", None)
@@ -55,23 +84,35 @@ class BaseModel:
                                          dictionary)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        """
+        Updates the 'updated_at' attribute with the current datetime
+        and saves the instance to storage.
+        """
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """
+        Converts the BaseModel instance to a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the BaseModel instance.
+        """
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+            new_dict["created_at"] = new_dict["created_at"].strftime(
+                time_format)
         if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(
+                time_format)
         new_dict["__class__"] = self.__class__.__name__
         new_dict.pop("_sa_instance_state", None)
 
         return new_dict
 
     def delete(self):
-        """delete the current instance from the storage"""
+        """
+        Deletes the current instance from storage (if applicable).
+        """
         models.storage.delete(self)
--- models/user.py
+++ models/user.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
+"""Module that defines the User class."""
+
 import hashlib
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
+
+# Constants for configuration
+STORAGE_TYPE = getenv("HBNB_TYPE_STORAGE", "fs")
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
+    """
+    User class represents users in the application.
+
+    This class defines the User model, including properties and methods for
+    working with user data.
+
+    Attributes:
+        __tablename__ (str): The name of the database table for User objects.
+        email (str): The email address associated with the user.
+        _password (str): The hashed password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        places (Relationship): A relationship to the Place model.
+        reviews (Relationship): A relationship to the Review model.
+    """
+
+    if STORAGE_TYPE == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         _password = Column("password", String(128), nullable=False)
@@ -20,7 +39,6 @@ class User(BaseModel, Base):
                               cascade="all, delete-orphan")
         reviews = relationship("Review", backref="user",
                                cascade="all, delete-orphan")
-
     else:
         email = ""
         _password = ""
@@ -28,22 +46,43 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """
+        Initialize a new User instance.
+
+        Args:
+            *args: Positional arguments.
+            **kwargs: Keyword arguments.
+        """
         super().__init__(*args, **kwargs)
 
     @property
     def password(self):
-        """The password property"""
+        """
+        The password property.
+
+        Returns:
+            str: The hashed password of the user.
+        """
         return self._password
 
     @password.setter
     def password(self, value):
-        """Set password"""
+        """
+        Set the user's password.
+
+        Args:
+            value (str): The plaintext password to be hashed.
+        """
         self._password = hashlib.md5(value.encode('utf8')).hexdigest()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """
+        Convert the User instance to a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the User instance.
+        """
         new_dict = super().to_dict()
-        if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+        if STORAGE_TYPE == "db":
             new_dict.pop("password", None)
         return new_dict
